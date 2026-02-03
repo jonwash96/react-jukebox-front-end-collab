1204f55bafaa47f4a39096cdc45a79cf2242daf7
JON Washington <jonwashPro@gmail.com>
2026-02-03 15:24:56 -0600

    Setup trackService

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import * as trackService from './services/trackService.js'
 
 function App() {
-	const [count, setCount] = useState(0)
+    const [tracks, setTracks] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("@App | RUN useEffect")
+            try {
+                const data = await trackService.showOne("6981358a91d23e8ab9c008e8");
+                console.log(" Data: ", data)
+                setTracks([...tracks, data])
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchData()
+    },[])
+
 
 	return (
 		<>
-			<h1>Hello, World!</h1>
+			<h1>Basic MVP</h1>
+            <p>If the front end can talk to the back end and a valid trak id is placed in the showOne function call; Then the following fields should display on the page.</p>
+            <h4>Name: {tracks[0]?.title}</h4>
+            <p>Artist: {tracks[0]?.artist}</p>
 		</>
 	)
 }
---
+++ src/services/trackService.js
@@ -0,0 +1,64 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+export async function index() {
+    return fetch(BASE_URL)
+        .then(response => response.json())
+        .then(data => {
+            console.log("@trackSvc.index: ", data)
+            return data
+        })
+}
+
+export async function showOne(id) {
+    try {
+        const res = await fetch(BASE_URL+'/'+id, {
+            method:'GET',
+            headers:{"Content-Type":"application/json"},
+        })
+        if (!res) throw new Error("error")
+        return await res.json();
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export async function create(track) {
+    try {
+        const res = await fetch(BASE_URL, {
+            method:'POST',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(track)
+        })
+
+        return await res.json().track;
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export async function update(track) {
+    try {
+        const res = await fetch(`${BASE_URL}/${track._id}`, {
+            method:'PUT',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(track)
+        });
+
+        return await res.json().updatedTrack;
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export async function deleteTrack(id) {
+    try {
+        const res = await fetch(BASE_URL+'/'+id, {
+            method:'DELETE',
+            headers:{"Content-Type":"application/json"},
+        });
+
+        return await res.json().deletedTrack
+    } catch (err) {
+        console.error(err)
+    }
+}
