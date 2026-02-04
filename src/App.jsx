abd0c7bff363b0f1fe1c781395d3bc2cb4eeb0a1
Adam Myers <adammyers002@gmail.com>
2026-02-04 09:10:14 -0600

    added controlled TrackForm component that creates tracks via trackService

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import * as trackService from './services/trackService.js'
+import { useState, useEffect } from "react";
+import "./App.css";
+import * as trackService from "./services/trackService.js";
+import TrackForm from "./components/TrackForm";
 
 function App() {
-    const [tracks, setTracks] = useState([]);
+  const [tracks, setTracks] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            console.log("@App | RUN useEffect")
-            try {
-                const data = await trackService.showOne("6981358a91d23e8ab9c008e8");
-                console.log(" Data: ", data)
-                setTracks([...tracks, data])
-            } catch (err) {
-                console.error(err)
-            }
-        }
-        fetchData()
-    },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("@App | RUN useEffect");
+      try {
+        const data = await trackService.showOne("6981358a91d23e8ab9c008e8");
+        console.log(" Data: ", data);
+        setTracks([...tracks, data]);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
 
-
-	return (
-		<>
-			<h1>Basic MVP</h1>
-            <p>If the front end can talk to the back end and a valid trak id is placed in the showOne function call; Then the following fields should display on the page.</p>
-            <h4>Name: {tracks[0]?.title}</h4>
-            <p>Artist: {tracks[0]?.artist}</p>
-		</>
-	)
+  return (
+    <>
+      <h1>Basic MVP</h1>
+      <p>
+        If the front end can talk to the back end and a valid trak id is placed
+        in the showOne function call; Then the following fields should display
+        on the page.
+      </p>
+      <h4>Name: {tracks[0]?.title}</h4>
+      <p>Artist: {tracks[0]?.artist}</p>
+      <TrackForm />
+    </>
+  );
 }
 
-export default App
+export default App;
