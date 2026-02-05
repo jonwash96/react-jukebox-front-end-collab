fd038677d8449cf8c82ece43bbbe096df5720438
Adam Myers <adammyers002@gmail.com>
2026-02-04 20:07:04 -0600

    change trackform function to have handler functions for app.jsx and implement useEffect

--- src/components/TrackForm/TrackForm.jsx
+++ src/components/TrackForm/TrackForm.jsx
@@ -1,41 +1,34 @@
-import { useState } from "react";
-import * as trackService from '../../services/trackService'
+import { useState, useEffect } from "react";
 
-export default function TrackForm({tracks}) {
-  // hold all the values from the form inputs
+export default function TrackForm({
+  selected, // track to edit or null
+  setSelected, // allows us to exit edit mode
+  handleCreate, // function from App.jsx for creating
+  handleUpdate, // function from App.jsx for updating
+}) {
+  // this holds the values of our inputs (controlled form)
   const [formData, setFormData] = useState({
     title: "",
     artist: "",
   });
 
-  // update formData
-  // runs everytime the user types in an input
-  function handleChange(e) {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  // runs when form is submitted
-  async function handleSubmit(e) {
-    e.preventDefault(); // stops page from refreshing
-
-    try {
-      // send formData to the backend using the service file
-      const createdTrack = await trackService.create({ ...formData });
-
-      console.log("Track successfully created:", createdTrack);
-
-      // after submit clear the form for next the entry
+  // this runs whenever "selected" changes
+  // if selected has a track -> prefill inputs (edit mode)
+  // if selected is null -> clear inputs (create mode)
+  useEffect(() => {
+    if (selected) {
+      setFormData({
+        title: selected.title || "",
+        artist: selected.artist || "",
+        _id: selected._id, // important for update
+      });
+    } else {
       setFormData({
         title: "",
         artist: "",
       });
-    } catch (error) {
-      console.log("Error creating track:", error);
     }
-  }
+  }, [selected]);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -64,4 +57,4 @@ export default function TrackForm({tracks}) {
       <button type="submit">Create Track</button>
     </form>
   );
-};
+}
