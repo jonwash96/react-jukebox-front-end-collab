0fa4206bfd726c9647d88dc05c35d8d88efea2c4
JON Washington <jonwashPro@gmail.com>
2026-02-06 16:53:34 -0600

    Pt1: Manual merge of changes from jw/createButton

--- src/App.jsx
+++ src/App.jsx
@@ -6,164 +6,162 @@ import NowPlaying from "./components/NowPlaying/NowPlaying.jsx";
 import TrackForm from "./components/TrackForm/TrackForm.jsx";
 
 function App() {
-  const [tracks, setTracks] = useState([]);
-  const initNowPlaying = {
-    title: "",
-    artist: "",
-    duration: "0:00",
-    albumArt: "",
-  };
-  const [nowPlaying, setNowPlaying] = useState(initNowPlaying);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [selected, setSelected] = useState(null);
-  const [showEditModal, setShowEditModal] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log("@App | RUN useEffect");
-      try {
-        const data = await trackService.index();
-        // console.log("@App | Data: ", data)
-        setTracks(data);
-        if (data.length > 0) {
-          handleSelectSong(data[0]);
-          setIsPlaying(false);
-        }
-      } catch (err) {
-        console.error(err);
-      }
+    const [tracks, setTracks] = useState([]);
+    const initNowPlaying = {
+        title: "",
+        artist: "",
+        duration: "0:00",
+        albumArt: "",
     };
-    fetchData();
-  }, []);
+    const [nowPlaying, setNowPlaying] = useState(initNowPlaying);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const [showEditModal, setShowEditModal] = useState(false);
 
-  // Delete a track
-  const handleDelete = async (id) => {
-    console.log("@handleDelete", id);
-    try {
-      const deletedTrack = await trackService.deleteTrack(id);
-      if (!deletedTrack)
-        throw new Error("Delete Track Failed! Please try again later.");
-      setTracks(tracks.filter((track) => track._id !== id));
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("@App | RUN useEffect");
+            try {
+                const data = await trackService.index();
+                // console.log("@App | Data: ", data)
+                setTracks(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetchData();
+    }, []);
 
-      // if we delete the one playing, reset nowPlaying
-      if (nowPlaying?._id === id) {
-        setNowPlaying(initNowPlaying);
-        setIsPlaying(false);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    // Delete a track
+    const handleDelete = async (id) => {
+        console.log("@handleDelete", id);
+        try {
+            const deletedTrack = await trackService.deleteTrack(id);
+            if (!deletedTrack)
+                throw new Error("Delete Track Failed! Please try again later.");
+            setTracks(tracks.filter((track) => track._id !== id));
 
-  /**
-   * When edit is clicked in Tracklist:
-   * - store track in selected
-   * - show TrackForm
-   */
-  const handleEdit = (track) => {
-    // console.log("@handleEdit", track);
-    setSelected(track);
-    setShowEditModal(true);
-  };
+            // if we delete the one playing, reset nowPlaying
+            if (nowPlaying?._id === id) {
+                setNowPlaying(initNowPlaying);
+                setIsPlaying(false);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  /**
-   * Create a new track:
-   * - call API via trackservice.create
-   * - update local state so it appears immediately
-   * - close form
-   */
-  const handleCreate = async (trackData) => {
-    try {
-      const createdTrack = await trackService.create(trackData);
-      if (!createdTrack)
-        throw new Error("Create Track Failed! Please try again.");
+    /**
+     * When edit is clicked in Tracklist:
+     * - store track in selected
+     * - show TrackForm
+     */
+    const handleEdit = (track) => {
+        // console.log("@handleEdit", track);
+        setSelected(track);
+        setShowEditModal(true);
+    };
 
-      setTracks((prev) => [...prev, createdTrack]);
+    const showCreateForm = () => setShowEditModal(true);
 
-      // close form
-      setSelected(null);
-      setShowEditModal(false);
-    } catch (error) {
-      console.error(err);
-    }
-  };
+    /**
+     * Create a new track:
+     * - call API via trackservice.create
+     * - update local state so it appears immediately
+     * - close form
+     */
+    const handleCreate = async (trackData) => {
+        try {
+            const createdTrack = await trackService.create(trackData);
+            if (!createdTrack)
+                throw new Error("Create Track Failed! Please try again.");
 
-  /**
-   * Update existing track:
-   * - call API via trackService.update
-   * - replace updated track in local state
-   * - close form
-   */
-  const handleUpdate = async (track) => {
-    // console.log("@handleUpdate", id);
-    try {
-      const updatedTrack = await trackService.update(track);
-      if (!updatedTrack)
-        throw new Error("Update Track Failed! Please try again later.");
+            setTracks((prev) => [...prev, createdTrack]);
 
-      // replace the old track with the updated one in state
-      setTracks((prev) =>
-        prev.map((t) => (t._id === updatedTrack._id ? updatedTrack : t)),
-      );
+            // close form
+            setSelected(null);
+            setShowEditModal(false);
+        } catch (error) {
+            console.error(err);
+        }
+    };
 
-      // keep nowPlaying synced if you edited the track currently playing
-      if (nowPlaying?._id === updatedTrack._id) {
-        handleSelectSong(updatedTrack);
-      }
+    /**
+     * Update existing track:
+     * - call API via trackService.update
+     * - replace updated track in local state
+     * - close form
+     */
+    const handleUpdate = async (track) => {
+        // console.log("@handleUpdate", id);
+        try {
+            const updatedTrack = await trackService.update(track);
+            if (!updatedTrack)
+                throw new Error("Update Track Failed! Please try again later.");
 
-      // close form
-      setSelected(null);
-      setShowEditModal(false);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+            // replace the old track with the updated one in state
+            setTracks((prev) =>
+                prev.map((t) => (t._id === updatedTrack._id ? updatedTrack : t)),
+            );
 
-  // track "play" selection
-  const handleSelectSong = (track) => {
-    setNowPlaying({
-      ...track,
-      duration: track?.duration || "0:00",
-      albumArt: track?.albumArt || "",
-    });
-    setIsPlaying(true);
-  };
+            // keep nowPlaying synced if you edited the track currently playing
+            if (nowPlaying?._id === updatedTrack._id) {
+                handleSelectSong(updatedTrack);
+            }
 
-  return (
-    <main>
-      <h1>Jukebox lite</h1>
+            // close form
+            setSelected(null);
+            setShowEditModal(false);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-      {/* Main UI shown when form is not visible */}
-      {!showEditModal && (
-        <Tracklist
-          tracks={tracks}
-          crud={{ handleDelete, handleEdit }}
-          handleSelectSong={handleSelectSong}
-          nowPlaying={nowPlaying}
-        />
-      )}
+    // track "play" selection
+    const handleSelectSong = (track) => {
+        setNowPlaying({
+            ...track,
+            duration: track?.duration || "0:00",
+            albumArt: track?.albumArt || "",
+        });
+        setIsPlaying(true);
+    };
 
-      {!showEditModal && (
-        <NowPlaying
-          track={nowPlaying}
-          isPlaying={isPlaying}
-          setIsPlaying={setIsPlaying}
-          crud={{ handleDelete, handleEdit }}
-        />
-      )}
+    return (
+        <main>
+            <h1>Jukebox lite</h1>
+            <button type="button" id="create-button" onClick={showCreateForm}>➕</button>
+            {/* Main UI shown when form is not visible */}
+            {!showEditModal && (
+                <Tracklist
+                    tracks={tracks}
+                    crud={{ handleDelete, handleEdit }}
+                    handleSelectSong={handleSelectSong}
+                    nowPlaying={nowPlaying}
+                />
+            )}
 
-      {/* Form shown when showEditModal is true */}
-      {showEditModal && (
-        <TrackForm
-          track={selected}
-          setSelected={setSelected}
-          handleCreate={handleCreate}
-          handleUpdate={handleUpdate}
-          closeForm={() => setShowEditModal(false)}
-        />
-      )}
-    </main>
-  );
+            {nowPlaying.title && (
+                <NowPlaying
+                    track={nowPlaying}
+                    isPlaying={isPlaying}
+                    setIsPlaying={setIsPlaying}
+                    crud={{ handleDelete, handleEdit }}
+                />
+            )}
+
+            {/* Form shown when showEditModal is true */}
+            {showEditModal && (
+                <TrackForm
+                    track={selected}
+                    setSelected={setSelected}
+                    handleCreate={handleCreate}
+                    handleUpdate={handleUpdate}
+                    closeForm={() => setShowEditModal(false)}
+                />
+            )}
+        </main>
+    );
 }
 
 export default App;
